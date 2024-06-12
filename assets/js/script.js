b6819d182ed965fc8d1ee0b52d58c6ec15b3fb22
alutterbein <alutterbein@gmail.com>
2024-06-11 23:45:38 -0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,82 @@
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
+
+
+var correctCounter = 0;
+var incorrectCounter = 0;
+var timer;
+var timerCount;
+
+var questions = [
+    {
+      question: "Who invented JavaScript?",
+      answers: {
+        a: "Douglas Crockford",
+        b: "Sheryl Sandberg",
+        c: "Brendan Eich"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Which one of these is a JavaScript package manager?",
+      answers: {
+        a: "Node.js",
+        b: "TypeScript",
+        c: "npm"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Which tool can you use to ensure code quality?",
+      answers: {
+        a: "Angular",
+        b: "jQuery",
+        c: "RequireJS",
+        d: "ESLint"
+      },
+      correctAnswer: "d"
+    }
+  ];
+
+
+for(var i=0; i <questions.length; i++){
+  var response = window.prompt(questions[i.prompt]);
+if(response== questions[i].answer){
+  correctCounter++;
+}
+else {
+  incorrectCounter++;
+}
+}
+alert("You answered" + correctCounter + "/" + questions.length)
+
+function buildQuiz(){
+var output =[];
+myQuestions.forEach
+
+
+}
+
+// The setTimer function starts and stops the timer and triggers winGame() and loseGame()
+function startTimer() {
+  // Sets timer
+  timer = setInterval(function() {
+    timerCount--;
+    timerElement.textContent = timerCount;
+    if (timerCount >= 0) {
+      // Tests if win condition is met
+      if (isWin && timerCount > 0) {
+        // Clears interval and stops timer
+        clearInterval(timer);
+        winGame();
+      }
+    }
+    // Tests if time has run out
+    if (timerCount === 0) {
+      // Clears interval
+      clearInterval(timer);
+      loseGame();
+    }
+  }, 1000);
+}
+
